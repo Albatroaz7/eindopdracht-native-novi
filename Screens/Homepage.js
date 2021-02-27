54cc4971a5f7b7369776ba88d14fcdd430ef14c5
Albatroaz7 <sambarnhoorn@gmail.com>
2021-02-27 16:18:01 +0100

    Added styling and a search option on the movie page.

--- Screens/Homepage.js
+++ Screens/Homepage.js
@@ -1,36 +1,68 @@
 import React from 'react';
-import {StyleSheet, Text, View, Button} from "react-native";
+import {StyleSheet, Text, View, TouchableOpacity, ImageBackground} from "react-native";
 import {StatusBar} from "expo-status-bar";
+
+const image = { uri: "https://1.bp.blogspot.com/-yeSWKghDRWQ/Xqs81nJtWXI/AAAAAAAAH2Y/a9eRefLq3VUAWxriZD0XZ98UgpDaWRm-gCLcBGAsYHQ/w1200-h630-p-k-no-nu/Unblock%2BNetflix%2BUS%2BLibrary%2B%2526%2BStream%2BGlobal%2BNetflix%2BMovies%2B%2526%2BTV%2BShows.jpg" }
 
 export default function Homepage({ navigation }){
     return(
         <View style={styles.container}>
+            <ImageBackground source={image} style={styles.backgroundImage}>
             <Text style={styles.homeTitle}>Netflix</Text>
             <Text style={styles.homeText}>Welcome to the Netflix Movie app. If you will click on the button below, you'll find a list of movies. Whenever u click on one of the movies, you'll see a overview of the movie with all sorts of information!</Text>
-            <Button
+            <TouchableOpacity
+                style={styles.button}
                 title="View Movies"
                 onPress={() => navigation.navigate('Movies')}
-            />
+            >
+                <Text style={styles.buttonText}>Go to Movies</Text>
+            </TouchableOpacity>
             <StatusBar backgroundColor="red" />
+            </ImageBackground>
         </View>
+
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'black',
         alignItems: 'center',
-        justifyContent: 'center',
     },
     homeTitle: {
         fontWeight: 'bold',
-        fontSize: 25,
+        fontSize: 40,
         fontFamily: 'serif',
         color: 'red',
+        alignSelf: 'center',
+        justifyContent: 'flex-start',
     },
     homeText: {
-        color: 'white'
+        fontSize: 18,
+        color: 'white',
+        flex: 0.3,
+        backgroundColor: 'black',
+        borderWidth: 2,
+        borderColor: '#990000',
+        alignSelf: 'center',
+        paddingHorizontal: 30
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor: 'red',
+        margin: 20,
+        padding: 10,
+    },
+    buttonText: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        fontFamily: 'serif'
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: 'center'
     }
+
 });
 
--- Screens/Movies.js
+++ Screens/Movies.js
@@ -1,17 +1,19 @@
 import React, { useEffect, useState} from 'react';
-import { Text, View, FlatList, StyleSheet, Pressable} from "react-native";
+import { Text, View, FlatList, StyleSheet, Pressable, TextInput} from "react-native";
 import axios from 'axios';
 import MovieShow from "./MovieShow";
 
 export default function Movies({ navigation }){
     const [ movies, setMovies] = useState([]);
+    const [ searchTerm, setSearchTerm] = useState('')
 
     const options1 = {
         method: 'GET',
         url: 'https://unogsng.p.rapidapi.com/search',
         params: {
+            query: searchTerm,
             orderby: 'date',
-            limit: '100',
+            limit: '50',
             start_year: '2000',
             end_year: '2020',
         },
@@ -34,24 +36,34 @@ export default function Movies({ navigation }){
             }
         }
         getMovie()
-    }, [])
+    }, [searchTerm])
     console.log(movies);
 
     function goToMovie(item){
         navigation.navigate("MovieShow", { item: item})
     }
 
+    // const handleOnChange = (e) => {
+    //     setSearchTerm(e.target.value);
+    // }
+
     return <View style={styles.movieContainer}>
             <Text style={styles.movieTitle}>Movies:</Text>
+            <TextInput
+                style={styles.searchbox}
+                onChangeText={setSearchTerm}
+            />
 
-            <FlatList
+            <FlatList styles={styles.results}
                 data={movies}
                 renderItem={({ item }) => {
-                    return<Pressable onPress={() => goToMovie(item)}>
-                        <Text style={styles.listItem}>
+                    return<View key={item.id} style={styles.result}>
+                    <Pressable onPress={() => goToMovie(item)}>
+                        <Text style={styles.heading}>
                         {item.title}
                      </Text>
                     </Pressable>
+                    </View>
             }}
                 keyExtractor={item => item.id.toString()}
                 ItemSeparatorComponent={() => {
@@ -63,26 +75,38 @@ export default function Movies({ navigation }){
 
 const styles = StyleSheet.create({
     movieContainer: {
+        flex: 1,
         alignItems: 'center',
         backgroundColor: 'black',
     },
-    itemSeparator:{
-        borderBottomColor: 'red',
-        borderWidth: 2
+    results: {
+        flex: 1,
     },
-    listItem:{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        margin: 5,
-        fontSize: 15,
-        fontFamily: 'serif',
-        color: 'white'
+    result: {
+        flex: 1,
+        width: '100%',
+        marginBottom: 20
+    },
+    heading: {
+        color: 'black',
+        fontSize: 18,
+        fontWeight: '700',
+        padding: 20,
+        backgroundColor: '#990000'
     },
     movieTitle:{
-        color: 'red',
+        color: '#990000',
         fontWeight: 'bold',
         fontSize: 25,
         fontFamily: 'serif'
+    },
+    searchbox: {
+        fontSize: 20,
+        fontWeight: '300',
+        padding: 10,
+        width: '100%',
+        backgroundColor: '#4d4d4d',
+        borderRadius: 8,
+        marginBottom: 40
     }
 })
